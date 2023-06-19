ffa028e7fcefd0e267b2444b3e0a7079e0d0e27c
Gonzalez-Ignacio <ignaciogonzalez2306@gmail.com>
2023-06-18 23:56:04 -0300

    sidebar

--- PAGINA PRINCIPAL/startbootstrap-modern-business-gh-pages/js/scripts.js
+++ PAGINA PRINCIPAL/startbootstrap-modern-business-gh-pages/js/scripts.js
@@ -1,7 +1,12 @@
-/*!
-* Start Bootstrap - Modern Business v5.0.7 (https://startbootstrap.com/template-overviews/modern-business)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-modern-business/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+const body = document.querySelector('body'),
+      sidebar = body.querySelector('.sidebar'),
+      toggleIcon = body.querySelector('.toggle-icon'),
+      searchBtn = body.querySelector('.search-box');
+
+      toggleIcon.addEventListener('click', () => {
+        sidebar.classList.toggle('close');
+      });
+
+      searchBtn.addEventListener('click', () => {
+        sidebar.classList.remove('close');
+      });
