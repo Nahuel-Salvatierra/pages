ffbe7e691bcd89187d0f39da88fba7879d74c1d7
Gonzalez-Ignacio <ignaciogonzalez2306@gmail.com>
2023-07-13 19:31:23 -0300

    Se agrego Carouseles y Activar/Desactivar Sidebar

--- PAGINA PRINCIPAL/startbootstrap-modern-business-gh-pages/js/scripts.js
+++ PAGINA PRINCIPAL/startbootstrap-modern-business-gh-pages/js/scripts.js
@@ -1,12 +1,46 @@
-const body = document.querySelector('body'),
-      sidebar = body.querySelector('.sidebar'),
-      toggleIcon = body.querySelector('.toggle-icon'),
-      searchBtn = body.querySelector('.search-box');
+// Sidebar
+const body = document.querySelector('body');
+const sidebar = body.querySelector('.sidebar');
+const toggleIcon = body.querySelector('.toggle-icon');
+const searchBtn = body.querySelector('.search-box');
+const scrollThreshold = 60; // Cantidad de scroll en píxeles para mostrar el sidebar
 
-      toggleIcon.addEventListener('click', () => {
-        sidebar.classList.toggle('close');
-      });
+toggleIcon.addEventListener('click', () => {
+  sidebar.classList.toggle('close');
+});
 
-      searchBtn.addEventListener('click', () => {
-        sidebar.classList.remove('close');
-      });
+searchBtn.addEventListener('click', () => {
+  sidebar.classList.remove('close');
+});
+
+// Mostrar el sidebar al desplazarse hacia abajo
+function showSidebarOnScroll() {
+  if (window.scrollY > scrollThreshold) {
+    sidebar.classList.remove('hidden');
+  } else {
+    sidebar.classList.add('hidden');
+  }
+}
+window.addEventListener('scroll', showSidebarOnScroll);
+// Fin Sidebar
+
+// SwiperJS Card Carousel
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  loop: true,
+  grabCursor: true,
+  keyboard: {
+    enabled: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+});
+// Fin SwiperJS Card Carousel
